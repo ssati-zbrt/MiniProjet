843bb8772540a22fa0d112ab0170875849e9d056
ZOUBERT <fainoussatizoubert@gmail.com>
2025-09-25 17:25:36 +0200

    Projet n°1

---
+++ Projet n°1 Términé.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+// Phase 1: Addition de deux nombres
+
+/*
+    int x,y,a;
+    printf("Donnez la valeur de x, y \n");
+    scanf("%d %d",&x ,&y);
+    a=x+y;
+    printf("La somme vaut %d \n", a );
+    return 0;
+/*
+
+// Phase 2 Quatres opérations
+
+/*
+    int x,y,choix,a;
+    printf("Choisit 1 : addition, 2 : soustraction, 3 : multiplication, 4 : division entière\n");
+    scanf("%d",&choix);
+    printf("Donnez la valeur de x, y \n");
+    scanf("%d %d",&x ,&y);
+    if (choix ==1 ){
+        a = x + y;
+    }
+    if (choix ==2 ){
+        a = x - y;
+    }
+    if (choix ==3){
+        a = x * y;
+    }
+    if (choix ==4 ){
+        a = x / y;
+    }
+    printf ("Le resultat est: %d \n", a);
+*/
+
+// Phase 3 Calculs en boucle avec while
+
+/*
+    int x,y,choix,a;
+    while ( choix!=5){
+        printf("Choisit: \n 1 : addition,\n 2 : soustraction,\n 3 : multiplication,\n 4 : division entiere,\n 5: quitter,");
+        scanf("%d",&choix);
+        if (choix==5){
+            printf("Le programme s'arrete la\n");
+        }
+        else{
+        printf("Donnez la valeur de x, y \n");
+        scanf("%d %d",&x ,&y);
+            if (choix ==1){
+                a = x + y;
+            }
+            if (choix==2){
+                a = x - y;
+            }
+            if (choix==3){
+                a = x * y;
+            }
+            if (choix==4){
+                a = x / y;
+            }
+
+            printf ("Le resultat est: %d \n", a);
+        }
+    }
+*/
+}
